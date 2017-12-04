47f8fc0c40accc4061d0ba2701acd25259a18226
Desmond Duan <Desmond@Desmonds-MacBook-Pro.local>
2017-12-05 04:00:29 +0800

    change transport model func

---
+++ goshop-common-web/src/main/java/org/goshop/common/pojo/SysMap.java
@@ -0,0 +1,33 @@
+package org.goshop.common.pojo;
+
+/**
+ * Created by Desmond on 04/12/2017.
+ */
+public class SysMap {
+    private Object key;
+    private Object value;
+
+    public SysMap(){
+    }
+
+    public SysMap(Object key, Object value){
+        this.key = key;
+        this.value = value;
+    }
+
+    public Object getKey(){
+        return this.key;
+    }
+
+    public void setKey(Object key){
+        this.key = key;
+    }
+
+    public Object getValue(){
+        return this.value;
+    }
+
+    public void setValue(Object value){
+        this.value = value;
+    }
+}
